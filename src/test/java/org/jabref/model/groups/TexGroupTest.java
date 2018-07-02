c8780d7db1507eecc391eeab0ca77812c7087ec6
Alexandre Lindquist Franco <alfleoncio@gmail.com>
2018-07-02 19:06:08 -0300

    Add tests to TexGroup

--- src/test/java/org/jabref/model/groups/TexGroupTest.java
+++ src/test/java/org/jabref/model/groups/TexGroupTest.java
@@ -2,6 +2,8 @@ package org.jabref.model.groups;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Set;
 
 import org.jabref.logic.auxparser.DefaultAuxParser;
 import org.jabref.model.database.BibDatabase;
@@ -10,6 +12,7 @@ import org.jabref.model.util.DummyFileUpdateMonitor;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -33,5 +36,51 @@ public class TexGroupTest {
         notInAux.setCiteKey("NotInAux2017");
 
         assertFalse(group.contains(notInAux));
+    }
+
+    @Test
+    public void deepCopyShouldBeEqual() throws Exception {
+        Path auxFile = Paths.get(TexGroupTest.class.getResource("paper.aux").toURI());
+        TexGroup group1 = new TexGroup("paper", GroupHierarchyType.INDEPENDENT, auxFile, new DefaultAuxParser(new BibDatabase()), new DummyFileUpdateMonitor());
+        AbstractGroup group2 = group1.deepCopy();
+
+        assertTrue(group1.equals(group2));
+    }
+
+    @Test
+    public void hashCodeMatchesObjectHash() throws Exception {
+        Path auxFile = Paths.get(TexGroupTest.class.getResource("paper.aux").toURI());
+        TexGroup group = new TexGroup("paper", GroupHierarchyType.INDEPENDENT, auxFile, new DefaultAuxParser(new BibDatabase()), new DummyFileUpdateMonitor());
+
+        int expected = Objects.hash(Objects.hash(group.getName(), group.getDescription(), group.getHierarchicalContext()), auxFile);
+        assertEquals(expected, group.hashCode());
+    }
+
+    @Test
+    public void groupToStringReturnsExpectedText() throws Exception {
+        Path auxFile = Paths.get(TexGroupTest.class.getResource("paper.aux").toURI());
+        DefaultAuxParser auxParser = new DefaultAuxParser(new BibDatabase());
+        DummyFileUpdateMonitor fileMonitor = new DummyFileUpdateMonitor();
+        TexGroup group = new TexGroup("paper", GroupHierarchyType.INDEPENDENT, auxFile, auxParser, fileMonitor);
+        BibEntry dummyEntry = new BibEntry();
+        group.contains(dummyEntry);
+        Set<String> keysUsedInAux = auxParser.parse(auxFile).getUniqueKeys();
+
+        String expected = "TexGroup{" +
+                "filePath=" + auxFile +
+                ", keysUsedInAux=" + keysUsedInAux +
+                ", auxParser=" + auxParser +
+                ", fileMonitor=" + fileMonitor +
+                "} " +
+                "AbstractGroup{" +
+                "name='" + group.getName() + '\'' +
+                ", context=" + group.getHierarchicalContext() +
+                ", color=" + group.getColor() +
+                ", isExpanded=" + group.isExpanded() +
+                ", description=" + group.getDescription() +
+                ", iconName=" + group.getIconName() +
+                '}';
+        assertEquals(expected, group.toString());
+
     }
 }
