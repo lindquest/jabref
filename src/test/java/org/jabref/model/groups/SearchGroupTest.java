76e99366b48a1912cc0da602869012a44e4e6951
Alexandre Lindquist Franco <alfleoncio@gmail.com>
2018-07-02 20:49:57 -0300

    Add tests to SearchGroup class

--- src/test/java/org/jabref/model/groups/SearchGroupTest.java
+++ src/test/java/org/jabref/model/groups/SearchGroupTest.java
@@ -2,12 +2,13 @@ package org.jabref.model.groups;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Objects;
 
 import org.jabref.model.entry.BibEntry;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchGroupTest {
@@ -51,5 +52,21 @@ public class SearchGroupTest {
         entries.add(entry3);
 
         assertTrue(group.containsAny(entries));
+    }
+
+    @Test
+    public void deepCopyShouldBeEqual() {
+        SearchGroup group = new SearchGroup("myExplicitGroup", GroupHierarchyType.INDEPENDENT, "anyfield=var*", true, true);
+        AbstractGroup group2 = group.deepCopy();
+
+        assertTrue(group.equals(group2));
+    }
+
+    @Test
+    public void hashCodeMatchesObjectHash() {
+        SearchGroup group = new SearchGroup("myExplicitGroup", GroupHierarchyType.INDEPENDENT, "anyfield=var*", true, true);
+
+        int expected = Objects.hash(group.getName(), group.getHierarchicalContext(), group.getSearchExpression(), group.isCaseSensitive(), group.isRegularExpression());
+        assertEquals(expected, group.hashCode());
     }
 }
