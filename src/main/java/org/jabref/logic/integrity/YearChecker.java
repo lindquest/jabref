a1d353a6a9790dc2b0bed0f5bc17aee3cb711a92
Alexandre Lindquist Franco <alfleoncio@gmail.com>
2018-07-14 19:09:59 -0300

    Change year check to numeric, based on min/max defined on java Calendar

--- src/main/java/org/jabref/logic/integrity/YearChecker.java
+++ src/main/java/org/jabref/logic/integrity/YearChecker.java
@@ -1,40 +1,31 @@
 package org.jabref.logic.integrity;
 
+import java.util.GregorianCalendar;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
-
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.strings.StringUtil;
 
 public class YearChecker implements ValueChecker {
 
-    private static final Predicate<String> CONTAINS_FOUR_DIGIT = Pattern.compile("([^0-9]|^)[0-9]{4}([^0-9]|$)")
-            .asPredicate();
-    private static final Predicate<String> ENDS_WITH_FOUR_DIGIT = Pattern.compile("[0-9]{4}$").asPredicate();
-    private static final String PUNCTUATION_MARKS = "[(){},.;!?<>%&$]";
+    private static final GregorianCalendar calendar = new GregorianCalendar();
 
     /**
-     * Checks, if the number String contains a four digit year and ends with it.
-     * Official bibtex spec:
-     * Generally it should consist of four numerals, such as 1984, although the standard styles
-     * can handle any year whose last four nonpunctuation characters are numerals, such as ‘(about 1984)’.
-     * Source: http://ftp.fernuni-hagen.de/ftp-dir/pub/mirrors/www.ctan.org/biblio/bibtex/base/btxdoc.pdf
-     */
+     * Checks if year value is numeric and valid within Gregorian Calendar limits
+     **/
     @Override
     public Optional<String> checkValue(String value) {
         if (StringUtil.isBlank(value)) {
             return Optional.empty();
         }
-
-        if (!CONTAINS_FOUR_DIGIT.test(value.trim())) {
-            return Optional.of(Localization.lang("should contain a four digit number"));
+        try {
+            if ((Integer.parseInt(value.trim()) < calendar.getMinimum(GregorianCalendar.YEAR))
+                    || (Integer.parseInt(value.trim()) > calendar.getMaximum(GregorianCalendar.YEAR))) {
+                return Optional.of(Localization.lang("should be a valid year (numbers only)"));
+            }
         }
-
-        if (!ENDS_WITH_FOUR_DIGIT.test(value.replaceAll(PUNCTUATION_MARKS, ""))) {
-            return Optional.of(Localization.lang("last four nonpunctuation characters should be numerals"));
-        }
-
+        catch (NumberFormatException e) {
+                return Optional.of(Localization.lang("should be a valid year (numbers only)"));
+            }
         return Optional.empty();
     }
 }
